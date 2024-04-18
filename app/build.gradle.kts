36b2c85ff6573a04a4777929e7d43d4a74c2f5dd
vavitam <tamvinhxuan123@gmail.com>
2024-04-18 21:56:48 +0700

    Update Main and Authen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,10 @@ plugins {
 }
 
 android {
+    buildFeatures {
+        viewBinding = true
+    }
+
     namespace = "com.example.taskmanager"
     compileSdk = 34
 
--- app/src/main/java/com/example/taskmanager/AuthenActivity.kt
+++ app/src/main/java/com/example/taskmanager/AuthenActivity.kt
@@ -2,21 +2,18 @@ package com.example.taskmanager
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.animation.AnimationUtils
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.taskmanager.databinding.ActivityMainBinding
+import com.example.taskmanager.databinding.ActivityAuthenBinding
 
-class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+class AuthenActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAuthenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityAuthenBinding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -24,13 +21,15 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        val animation = AnimationUtils.loadAnimation(applicationContext,R.anim.fade_in)
-        binding.imageLogo.startAnimation(animation)
-
-        val handler = Handler(Looper.getMainLooper())
-        handler.postDelayed({
-            val i = Intent(this,AuthenActivity::class.java)
+        binding.btnDangNhap.setOnClickListener {
+            val i = Intent(this, SignInActivity::class.java)
             startActivity(i)
-        },5000)
+        }
+
+        binding.btnDangKy.setOnClickListener {
+            val i = Intent(this, RegisterActivity::class.java)
+            startActivity(i)
+        }
+
     }
 }
