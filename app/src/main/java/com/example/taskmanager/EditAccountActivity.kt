da1e44297d7d58f7e75b602a63592a1feb3bb2fe
vavitam <tamvinhxuan123@gmail.com>
2024-04-21 11:33:50 +0700

    Edit Account

---
+++ app/src/main/java/com/example/taskmanager/EditAccountActivity.kt
@@ -0,0 +1,23 @@
+package com.example.taskmanager
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.taskmanager.databinding.ActivityEditAccountBinding
+
+class EditAccountActivity : AppCompatActivity() {
+    lateinit var binding: ActivityEditAccountBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityEditAccountBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
