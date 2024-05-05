2d58f497830d10490f0d3fee29e1c427e53eae70
vavitam <tamvinhxuan123@gmail.com>
2024-05-06 03:46:49 +0700

    image user

--- app/src/main/java/com/example/taskmanager/auth/RegisterActivity.kt
+++ app/src/main/java/com/example/taskmanager/auth/RegisterActivity.kt
@@ -2,16 +2,12 @@ package com.example.taskmanager.auth
 
 import android.content.Intent
 import android.net.Uri
-import android.nfc.Tag
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.text.TextUtils
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,15 +15,16 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.taskmanager.R
 import com.example.taskmanager.databinding.ActivityRegisterBinding
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.FirebaseStorage
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var binding: ActivityRegisterBinding
     lateinit var firebaseAuth: FirebaseAuth
     private var db = Firebase.firestore
+
+    private var selectedImageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +38,7 @@ class RegisterActivity : AppCompatActivity() {
         }
 
         binding.imageUserAccount.setOnClickListener {
-//            pickImage.launch("image/*")
+            pickImage.launch("image/*")
         }
 
         binding.btnDangKy.setOnClickListener {
@@ -60,50 +57,78 @@ class RegisterActivity : AppCompatActivity() {
             val phone = binding.edtPhone.text.toString()
             val pass = binding.edtPassword.text.toString()
             val confirmPass = binding.edtRePassword.text.toString()
+            val uid = FirebaseAuth.getInstance().currentUser!!.uid
+
+            binding.progressBar.visibility = View.VISIBLE
+            binding.linearBackground.visibility = View.GONE
 
             if (TextUtils.isEmpty(email)) {
-                binding.edtEmail.setError("Email is required")
+                Toast.makeText(this, "Email nhập sai", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             if (pass.length < 6) {
-                binding.edtEmail.setError("Pass is required")
+                Toast.makeText(this, "Mật khẩu tối thiểu 6 kí tự", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            if (selectedImageUri == null) {
+                Toast.makeText(this, "Ảnh chưa được chọn", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
 
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
                 if (pass == confirmPass) {
-
-                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
-                        // upload Profile picture
-
-
-                        if (it.isSuccessful) {
-                            val user = hashMapOf(
-                                "email" to email,
-                                "name" to name,
-                                "phone" to phone
-                            )
-
-                            val uid = FirebaseAuth.getInstance().currentUser!!.uid
-
-                            db.collection("users").document(uid).set(user)
-                                .addOnCompleteListener {
-                                    Toast.makeText(this,"Successfully added", Toast.LENGTH_SHORT).show()
-                                }
-
-                            dangNhap()
-                        } else {
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                    val storageRef = FirebaseStorage.getInstance().reference
+                    val imageRef = storageRef.child("images_user/${uid}.jpg")
+                    val uploadTask = imageRef.putFile(selectedImageUri!!)
+                    uploadTask.addOnSuccessListener {
+                        imageRef.downloadUrl.addOnSuccessListener {downloadUrl ->
+                            saveUserToFirestore(pass, email, phone, name, uid, downloadUrl.toString())
                         }
+                    } .addOnFailureListener {
+                        Toast.makeText(this, "Hinh anh that bai", Toast.LENGTH_SHORT).show()
                     }
+
                 } else {
-                    Toast.makeText(this, "Password is not matching", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Mật khẩu không khớp", Toast.LENGTH_SHORT).show()
                 }
             } else {
-                Toast.makeText(this, "Empty Fields Are not Allowed", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Chưa điền đầy đủ thông tin", Toast.LENGTH_SHORT).show()
             }
 
+            binding.progressBar.visibility = View.GONE
+            binding.linearBackground.visibility = View.VISIBLE
+        }
+    }
+
+    private fun saveUserToFirestore(pass: String, email: String, phone: String, name: String, uid: String, downloadUrl: String) {
+        firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {task ->
+            if (task.isSuccessful) {
+                val user = hashMapOf(
+                    "email" to email,
+                    "name" to name,
+                    "phone" to phone,
+                    "image" to downloadUrl
+                )
+
+                db.collection("users").document(uid).set(user)
+                    .addOnCompleteListener {
+                        Toast.makeText(this,"Đăng ký thành công", Toast.LENGTH_SHORT).show()
+                    }
+
+                dangNhap()
+            } else {
+                Log.e("REGISTER_ERROR", "Error creating user", task.exception)
+                Toast.makeText(this, "Error: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private val pickImage = registerForActivityResult(ActivityResultContracts.GetContent()) {
+        if (it != null) {
+            binding.imageUserAccount.setImageURI(it)
+            selectedImageUri = it
         }
     }
 
